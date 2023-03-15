7bd94a5e0d3e7b9841a2688800c22a2fb2f73ef8
garvit-368 <87135929+garvit-368@users.noreply.github.com>
2023-03-15 12:01:06 +0545

    Add files via upload

--- AmnilExercise/CRUD/script.js
+++ AmnilExercise/CRUD/script.js
@@ -1,9 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
 var selectedRow = null;
 
 //Show Alerts
 function showAlert(message, className){
     const div = document.createElement("div");
-    div.className = 'alert alert-${className}';
+    div.className = `alert alert-${className}`;
 
     div.appendChild(document.createTextNode(message));
     const container = document.querySelector(".container");
@@ -35,33 +36,31 @@ document.querySelector("#student-form").addEventListener("submit", (e) =>{
         showAlert("Please fill in all fields", "danger");
     }
     else{
-        if(selectedRow == null)
-        const list = document.querySelector("#student-list");
-        const row = document.createComment("tr");
+        if(selectedRow == null) {
+            const list = document.querySelector("#student-list");
+            const row = document.createElement("tr");
 
-        row.innerHTML = `
-        <td>${firstName}</td>
-        <td>${lastName}</td>
-        <td>${rollNo}</td>
-        <td>
-        <a href="#" class="btn btn-warning btn-sm edit">Edit</a>
-        
-        <a href="#" class="btn btn-warning btn-danger delete">Delete</a>
-        `;
-        list.appendChild(row);
-        selectedRow = null;
-        showAlert("Student Added", "success");
-    }
-    else{
-        selectedRow.children[0].textContent = firstName;
-        selectedRow.children[1].textContent = lastName;
-        selectedRow.children[2].textContent = rollNo;
-        selectedRow = null;
-        showAlert("Student Info Edited", "info");
+            row.innerHTML = `
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${rollNo}</td>
+            <td>
+            <a href="#" class="btn btn-warning btn-sm edit">Edit</a>
+            <a href="#" class="btn btn-warning btn-danger delete">Delete</a>
+            </td>
+            `;
+            list.appendChild(row);
+            showAlert("Student Added", "success");
+        } else {
+            selectedRow.children[0].textContent = firstName;
+            selectedRow.children[1].textContent = lastName;
+            selectedRow.children[2].textContent = rollNo;
+            selectedRow = null;
+            showAlert("Student Info Edited", "info");
         }
 
         clearFields();
-    
+    }
 });
 
 // Edit Data
@@ -74,16 +73,15 @@ document.querySelector("#student-list").addEventListener("click", (e) =>{
         document.querySelector("#lastName").value = selectedRow.children[1].textContent;
         document.querySelector("#rollNo").value = selectedRow.children[2].textContent;
     }
-}
-
-
+});
 
 // Delete Data
 
 document.querySelector("#student-list").addEventListener("click", (e) =>{
-   target = e.target;
+    const target = e.target;
    if(target.classList.contains("delete")){
-    target.ParentElement.ParentElement.remove();
+    target.parentElement.parentElement.remove();
     showAlert("Student Data Deleted", "danger");
    }
+});
 });
