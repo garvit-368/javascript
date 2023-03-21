dd91ccfacb7ca5eff67767da81d5e0b89fd9242f
garvit-368 <87135929+garvit-368@users.noreply.github.com>
2023-03-21 22:53:50 +0545

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import BookList from '../../booklist-app/src/BookList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BookList />
+
+          
+        
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ BookList.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+function BookList() {
+  const [books, setBooks] = useState([
+    { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
+    { title: 'To Kill a Mockingbird', author: 'Harper Lee' },
+    { title: '1984', author: 'George Orwell' },
+  ]);
+
+  return (
+    <div>
+      <h1>Book List</h1>
+      <ul>
+        {books.map((book, index) => (
+          <li key={index}>
+            <strong>{book.title}</strong> by {book.author}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+
